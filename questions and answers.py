d836528b768d661b7711171ce0171cc6ef3429f7
Luca Nogueira de Souza <lucanogueira.desouza@gmail.com>
2025-04-21 22:48:24 -0300

    Update questions and answers.py

--- questions and answers.py
+++ questions and answers.py
@@ -1,27 +1,36 @@
+import os
+from time import sleep 
+
 print (
     "MATH QUIZ V1.0\n"
 )
-
 print (
     "-=-"*10 , "\n"
 )
-
 correct_answers = 0
 wrong_answers = 0 
-
 def questions(x,y): 
     global correct_answers , wrong_answers
     if x == y:
         correct_answers += 1
+        clear_screen()
         return (
-            "YOU GOT IT RIGHT!!"
+            "YOU GOT IT RIGHT!"
         )
+
     elif x != y: 
         wrong_answers += 1 
+        clear_screen()
         return (
             "YOU WERE WRONG!"
         )
-        
+    
+def clear_screen(): 
+    os.system("Cls")
+
+def sleep_screen(): 
+    sleep(1)
+
 question_1 = (print(
     "QUESTION 1) WHAT IS 2+2?\nA) 2\nB) 4\nC) 6"
     ))
@@ -30,11 +39,12 @@ person_answer1 = str(input(
     "TYPE YOUR ANSWER: "
 )).upper().strip()
 print (questions(person_answer1,correct_answer1))
+sleep_screen()
+clear_screen()
 
 print (
     "-=-"*10
 )
-
 question_2 = (print(
     "QUESTION 2) WHAT IS 7/7?\nA) 2\nB) 8\nC) 1"
     ))
@@ -43,6 +53,8 @@ person_answer2 = str(input(
     "TYPE YOUR ANSWER: "
 )).upper().strip()
 print (questions(person_answer2,correct_answer2))
+sleep_screen()
+clear_screen()
 
 print (
     "-=-"*10
@@ -56,13 +68,9 @@ person_answer3 = str(input(
     "TYPE YOUR ANSWER: "
 )).upper().strip()
 print (questions(person_answer3,correct_answer3))
+sleep_screen()
+clear_screen()
 
 print (
-    f"IT'S OVER!\nSUCCESSES: {correct_answers}\nERRORS: {wrong_answers}"
+    f"FINAL CLASSIFICATION:\n\nSUCCESSES: {correct_answers}\nERRORS: {wrong_answers}"
 )
-
-
-
-
-
-
